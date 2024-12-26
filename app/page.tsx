431bdc8b4f63701ca1f053d64f54df1850e7cb1d
kubotatsuki <kubotatsuki.official@gmail.com>
2024-12-27 00:01:14 +0800

    update name

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ export default function Page() {
   return (
     <section>
       <h1 className="mb-8 text-2xl font-semibold tracking-tighter">
-        Kubo Tatsuki
+        ShanJianQing
       </h1>
       <p className="mb-4">
         photographer & programmer & designer
